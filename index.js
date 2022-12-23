2587bb0abfa5a578b5a98fd69ae0ef38fc2fd2c4
Rxxbm <rubemcorrea0@gmail.com>
2022-12-23 02:05:42 -0200

    Estilização e utilização de partials

--- index.js
+++ index.js
@@ -6,32 +6,12 @@ const app = express();
 app.set("view engine", "ejs");
 //configurando acesso aos arquivos estaticos(css, imagens e etc)
 app.use(express.static("public"));
-app.get("/:nome/:sexo/:idade", (req, res) => {
-  var nome = req.params.nome;
-  var sexo = req.params.sexo;
-  var idade = req.params.idade;
-  var produtos = [
-    { nome: "batata", preco: 12 },
-    { nome: "carne", preco: 16 },
-    { nome: "miojo", preco: 2.5 },
-    { nome: "lasanha", preco: 7.2 },
-  ];
-  if (nome && sexo && idade) {
-    console.log("Entrei no true");
-    res.render("index", {
-      nome: nome,
-      idade: idade,
-      sexo: sexo,
-      produtos: produtos,
-    });
-  } else {
-    console.log("Entrei aqui");
-    res.render("index", {
-      nome: "Nenhum nome preenchido",
-      idade: "null",
-      sexo: "null",
-    });
-  }
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 });
 app.listen(8080, () => {
   console.log("app rodando");
